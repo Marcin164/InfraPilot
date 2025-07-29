790220f61c726f3588c88ed30c2024a6ee0b0163
Marcin Nowakowski <1999nowakowskimarcin@gmail.com>
2025-07-30 01:49:03 +0200

    add user info

--- frontend/src/Components/Details/UserInfo.tsx
+++ frontend/src/Components/Details/UserInfo.tsx
@@ -1,24 +1,48 @@
-import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCheckCircle,
+  faEnvelope,
+  faHome,
+  faPhone,
+  faPooStorm,
+  faSignsPost,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
 
 type Props = {};
 
 const UserInfo = (props: Props) => {
   return (
-    <div>
-      <div>
-        <img />
-        <div>
-          <div>Marcin Nowakowski</div>
-          <div>
+    <div className="w-full bg-[#FFFFFF] shadow-xl rounded-[10px] p-4">
+      <div className="flex items-center">
+        <img src="#" className="w-[85px] h-[85px]" />
+        <div className="px-4">
+          <div className="font-extrabold text-[#3C3C3C] text-[20px] pb-2">
+            Marcin Nowakowski
+          </div>
+          <div className="text-[18px] text-[#30A712]">
             <FontAwesomeIcon icon={faCheckCircle} />
-            <span>Enabled</span>
+            <span className="px-2">Enabled</span>
           </div>
         </div>
       </div>
+      <div className="py-4">
+        <div className="bg-[#2B9AE9] rounded-[10px] w-fit px-4 py-2 font-bold text-[#FFFFFF]">
+          Developers team
+        </div>
+      </div>
       <div>
-        <div>Developers team</div>
+        <div className="text-[#3C3C3C] text-[18px] font-bold pb-2">
+          <FontAwesomeIcon icon={faEnvelope} />
+          <span className="pl-3">nowakowskim@gmail.com</span>
+        </div>
+        <div className="text-[#3C3C3C] text-[18px] font-bold pb-2">
+          <FontAwesomeIcon icon={faPhone} />
+          <span className="pl-3">+48 776 730 238</span>
+        </div>
+        <div className="text-[#3C3C3C] text-[18px] font-bold pb-2">
+          <FontAwesomeIcon icon={faHome} />
+          <span className="pl-3">Ulicowa 64/20, Wroclaw</span>
+        </div>
       </div>
     </div>
   );
--- frontend/src/Pages/Main/Users/Details.tsx
+++ frontend/src/Pages/Main/Users/Details.tsx
@@ -1,6 +1,3 @@
-import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
 import UserInfo from "../../../Components/Details/UserInfo";
 
 type Props = {};
